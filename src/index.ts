ff936b3a36750b4193cea3afeaa9486ab0114775
ongaeshi <ongaeshi0621@gmail.com>
2022-03-29 22:19:49 +0900

    refactor: Move scripts to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello World!");
+import { DefaultRubyVM } from "ruby-head-wasm-wasi/dist/browser";
+
+let rubyVm:any = null;
+const main = async () => {
+  // Fetch and instntiate WebAssembly binary
+  const response = await fetch(
+    "https://cdn.jsdelivr.net/npm/ruby-head-wasm-wasi@0.2.0/dist/ruby.wasm"
+  );
+  const buffer = await response.arrayBuffer();
+  const module = await WebAssembly.compile(buffer);
+  const { vm } = await DefaultRubyVM(module);
+  rubyVm = vm;
+
+  rubyVm.printVersion();
+
+  runRubyScriptsInHtml();
+};
+
+export const runRubyScriptsInHtml = function () {
+  const input:HTMLTextAreaElement = <HTMLTextAreaElement>document.getElementById("input");
+  const output:HTMLTextAreaElement = <HTMLTextAreaElement>document.getElementById("output");
+  const result = rubyVm.eval(input.value);
+  output.value = result;
+};
+
+main();
